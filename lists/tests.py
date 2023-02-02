39e589d4fffddc0d4dd415825363cad6470da42d
sunrekay <wladkorostilev@yandex.ru>
2023-02-03 00:13:04 +0300

    Переадресация после POST показ всех элементов в шаблоне

--- lists/tests.py
+++ lists/tests.py
@@ -14,8 +14,27 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         responce = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', responce.content.decode())
-        self.assertTemplateUsed(responce, 'home.html')
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        responce = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(responce.status_code, 302)
+        self.assertEqual(responce['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        responce = self.client.get('/')
+
+        self.assertIn('itemey 1', responce.content.decode())
+        self.assertIn('itemey 2', responce.content.decode())
 
 class ItemModelTest(TestCase):
 
