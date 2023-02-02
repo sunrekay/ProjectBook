711afdbb4b6bca63f677cdf1a9ca78ce1696884c
sunrekay <wladkorostilev@yandex.ru>
2023-02-02 05:39:55 +0300

    ФТ теперь проверяет возможность ввести элемент списка

--- functional_test.py
+++ functional_test.py
@@ -2,6 +2,9 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -22,6 +25,26 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://127.0.0.1:8000/')
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Купить павлиньи перья')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+
         self.fail('Закончить тест')
 
 
