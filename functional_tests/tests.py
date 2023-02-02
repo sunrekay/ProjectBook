423227a684219a61b3edc8e7bb299a66514adf9f
sunrekay <wladkorostilev@yandex.ru>
2023-02-03 00:33:58 +0300

    Сделал functional_tests приложением, использовал LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,14 @@
-import unittest
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
 import time
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self) -> None:
         options = webdriver.FirefoxOptions()
@@ -26,7 +27,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://127.0.0.1:8000/')
+        self.browser.get(self.live_server_url)
 
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
@@ -50,7 +51,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         self.fail('Закончить тест')
-
-
-if __name__ == "__main__":
-    unittest.main(warnings='ignore')
