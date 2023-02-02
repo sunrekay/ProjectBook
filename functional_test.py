53e550886984819236a9a9d27300418c2042f055
sunrekay <wladkorostilev@yandex.ru>
2023-02-02 20:45:29 +0300

    Фиксация перед рефакторингом

--- functional_test.py
+++ functional_test.py
@@ -38,12 +38,15 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows),
-            "Новый элемент списка не появился в таблице"
-        )
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn('2: Сделать мушку из павлиньих перьев', [row.text for row in rows])
 
         self.fail('Закончить тест')
 
--- lists/tests.py
+++ lists/tests.py
@@ -7,6 +7,11 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         responce = self.client.get('')
+        self.assertTemplateUsed(responce, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        responce = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', responce.content.decode())
         self.assertTemplateUsed(responce, 'home.html')
