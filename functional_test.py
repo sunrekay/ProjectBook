71b370c66127ca5a9a508415b2e6ff500d37d425
sunrekay <wladkorostilev@yandex.ru>
2023-02-02 03:19:33 +0300

    Расширен первый ФТ, теперь испоьзует unittest

--- functional_test.py
+++ functional_test.py
@@ -1,24 +1,29 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 
 
-options = webdriver.FirefoxOptions()
-options.add_argument('--headless')
+class NewVisitorTest(unittest.TestCase):
 
-browser = webdriver.Firefox(
-    service= Service(GeckoDriverManager().install()),
-    options = options
-)
+    def setUp(self) -> None:
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--headless')
 
-try:
-    browser.get('http://127.0.0.1:8000/')
-    assert 'The install worked successfully! Congratulations!' in browser.title
-    print('Все нормально.')
+        self.browser = webdriver.Firefox(
+            service=Service(GeckoDriverManager().install()),
+            options=options
+        )
 
-except Exception as ex:
-    print('Лежит.')
+    def tearDown(self) -> None:
+        self.browser.quit()
 
-finally:
-    browser.close()
-    browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://127.0.0.1:8000/')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест')
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
